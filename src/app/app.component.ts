9ef4d7fedd4fe8d7559b13581d8c108b7ac47be6
NajhelyBS <s5anbandasanchez@gmail.com>
2023-01-26 08:50:17 -0500

    working in ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-27-calculadora';
+
+  suma = 0;
+  resta = 0;
+  multiplicacion = 0;
+  division = 0;
+
+  
+  Calcular(){
+    
+    const Num1 = parseInt((<HTMLInputElement>document.getElementById("num1")).value);
+
+    const Num2 = parseInt((<HTMLInputElement>document.getElementById("num2")).value);
+
+    let suma = Num1 + Num2;
+    this.suma = suma;
+    
+
+    let resta = Num1 - Num2;
+    this.resta = resta;
+
+
+    let multiplicacion = Num1*Num2;
+    this.multiplicacion = multiplicacion;
+
+    let division = Num1/Num2;
+    this.division = division;
+
+
+  }
 }
