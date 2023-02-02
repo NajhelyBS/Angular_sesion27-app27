8ff471dfe57706717f1eb84dc2dddd9e9456f537
NajhelyBS <s5anbandasanchez@gmail.com>
2023-02-02 01:28:01 -0500

    end commitE

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,61 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app-27-calculadora app is running!');
+    expect(compiled.querySelector('span')?.textContent).toContain('app-27-calculadora app is running!');
   });
+
+  it(`Test Suma se espera de resultado 10`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    const mockeData = {option:1, num1: 4, num2: 6};
+    const expectedResult = 10;
+
+    const result = mockeData.num1 + mockeData.num2;
+
+    expect(result).toEqual(expectedResult);
+    
+  });
+
+  it(`Test resta se espera de resultado 6`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    const mockeData = {option:2, num1: 12, num2: 6};
+    const expectedResult = 6;
+
+    const result = mockeData.num1 - mockeData.num2;
+
+    expect(result).toEqual(expectedResult);
+    
+  });
+
+
+  it(`Test multiplicacion se espera de resultado 20`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    const mockeData = {option:3, num1: 4, num2: 5};
+    const expectedResult = 20;
+
+    const result = mockeData.num1 * mockeData.num2;
+
+    expect(result).toEqual(expectedResult);
+    
+  });
+
+  it(`Test division se espera de resultado 5`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    const mockeData = {option:3, num1: 20, num2: 4};
+    const expectedResult = 5;
+
+    const result = mockeData.num1 / mockeData.num2;
+
+    expect(result).toEqual(expectedResult);
+    
+  });
+
 });
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,28 +12,54 @@ export class AppComponent {
   resta = 0;
   multiplicacion = 0;
   division = 0;
+  none:String = "Elija una operación"
 
+  num1:number = 0;
+  num2:number = 0;
   
+  option:String ="";
+  operacion:String = "5";
+
+  show = false;
+
   Calcular(){
-    
-    const Num1 = parseInt((<HTMLInputElement>document.getElementById("num1")).value);
 
-    const Num2 = parseInt((<HTMLInputElement>document.getElementById("num2")).value);
+    this.num1 = parseInt((<HTMLInputElement>document.getElementById("num1")).value);
 
-    let suma = Num1 + Num2;
-    this.suma = suma;
-    
-
-    let resta = Num1 - Num2;
-    this.resta = resta;
+    this.num2 = parseInt((<HTMLInputElement>document.getElementById("num2")).value);
 
 
-    let multiplicacion = Num1*Num2;
-    this.multiplicacion = multiplicacion;
+    this.option = (<HTMLSelectElement>document.getElementById('opciones')).value;
+      
+    this.operacion = this.option;
+  
 
-    let division = Num1/Num2;
-    this.division = division;
+    if (this.option == "1") {
+      let suma = this.num1 + this.num2;
+      this.suma = suma;
+
+     
+    } else if (this.option == "2") {
+      let resta = this.num1 - this.num2;
+      this.resta = resta;
 
 
+    } else if (this.option == "3") {
+      let multiplicacion = this.num1*this.num2;
+      this.multiplicacion = multiplicacion;
+
+
+    } else if (this.option == "4") {
+      let division = this.num1/this.num2;
+      this.division = division;
+
+    } else if (this.option == "5") {
+      this.show = true;
+    }
+   
   }
+
+
+
+
 }
